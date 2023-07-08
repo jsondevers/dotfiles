bae874b7852242932f9db51cd15136cf38674777
jsondevers <jasondevers@Js-M1.local>
2023-07-08 13:31:46 -0700

    maybe fixed?

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Personal config setup (really just to track neovim changes)
 
 `script.sh` is used to just transfer/copy files to this directory
+
+`chmod +x ./script.sh && ./script.sh`
--- script.sh
+++ script.sh
@@ -5,5 +5,5 @@ cp -r ~/.config/ ~/Desktop/dotfiles/.config
 cp ~/.bash_profile ~/Desktop/dotfiles
 
 # remove nested git repos
-git add .
-git rm -f --cached .config/nvim
+rm -rf .config/nvim/.git
+rm -rf .config/nvim.github
