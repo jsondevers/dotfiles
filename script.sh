15bed32459c0b445898f38942ec22a7413e2ec49
jsondevers <jasondevers@Js-M1.local>
2023-07-23 21:10:50 -0700

    mostly done

--- script.sh
+++ script.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
   
-# remove current files
-rm -rf ~/Desktop/dotfiles/.zshrc
-rm -rf ~/Desktop/dotfiles/.config/tmux
-rm -rf ~/Desktop/dotfiles/.config/alacritty
-rm -rf ~/Desktop/dotfiles/.oh-my-zsh
-rm -rf ~/Desktop/dotfiles/.config/nvim/lua/custom
-
 # copy files to this directory
 cp ~/.zshrc ~/Desktop/dotfiles/.zshrc
-cp -r  ~/.config/tmux/ ~/Desktop/dotfiles/.config/tmux
-cp -r ~/.config/alacritty/ ~/Desktop/dotfiles/.config/alacritty
-cp -r ~/.oh-my-zsh/ ~/Desktop/dotfiles/.oh-my-zsh
+cp ~/.config/tmux/tmux.conf ~/Desktop/dotfiles/.config/tmux/tmux.conf
 cp -r ~/.config/nvim/lua/custom/ ~/Desktop/dotfiles/.config/nvim/
-
+cp ~/.config/alacritty/alacritty.toml ~/Desktop/dotfiles/.config/alacritty/alacritty.toml 
 # remove git directories nested
 rm -rf ~/Desktop/dotfiles/.config/tmux/.git
 rm -rf ~/Desktop/dotfiles/.config/alacritty/.git
 rm -rf ~/Desktop/dotfiles/.oh-my-zsh/.git
-rm -rf ~/Desktop/dotfiles/.config/tmux/plugins/tmux-mem-cpu-load/.git
-rm -rf ~/Desktop/dotfiles/.config/tmux/plugins/tmux-mem-cpu-load/.github/
-rm -rf ~/Desktop/dotfiles/.config/alacritty/.github/
-rm -rf ~/Desktop/dotfiles/.oh-my-zsh/git
-rm -rf ~/Desktop/dotfiles/.oh-my-zsh/github
